551a8cc5ee3fa557c7b4a515b6f9ec34c06c2acf
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-06-30 03:29:26 +0500

    update

--- script.js
+++ script.js
@@ -8,12 +8,20 @@ let myFunction = () =>{
 
         console.log(data);
 
-    document.querySelector("#city_name").innerText = data.location.name + ", " + data.location.region;
-    document.querySelector("#feels_like").innerText = "Feels Like "+ data.current.feelslike_c + "°C";
+        document.querySelector("#weather_icon").src = data.current.condition.icon;
+    document.querySelector("#tempC").innerText = data.current.temp_c + "°C";
+    document.querySelector("#tempF").innerText = data.current.temp_f + "°F";
+
+
+    document.querySelector("#city_name").innerText = data.location.name + ", " + data.location.region+ ", " + data.location.country;
     document.querySelector("#weather_condition").innerText = data.current.condition.text;
-    document.querySelector("#weather_icon").src = data.current.condition.icon;
-    document.querySelector("#weather_condition").innerText = data.current.condition.text;
-    document.querySelector("#weather_condition").innerText = data.current.condition.text;
+
+    document.querySelector("#feels_like").innerText = "Feels Like: "+ data.current.feelslike_c + "°C";
+    document.querySelector("#wind_speed").innerText = "Wind Speed: " + data.current.wind_kph + "KP/H";
+    document.querySelector("#humidity").innerText = "Humidity: " + data.current.humidity + "%";
+
+
+    document.querySelector("#visibility").innerText = "Visibility : " + data.current.vis_km + "KM";
     document.querySelector("#weather_condition").innerText = data.current.condition.text;
     document.querySelector("#weather_condition").innerText = data.current.condition.text;    
     document.querySelector("#weather_condition").innerText = data.current.condition.text;
