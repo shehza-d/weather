8dcbd2b957879a2ca852fb4264a642b93ff8511a
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-06-30 02:58:34 +0500

    Done with API

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-// let myFunction = () =>{
-    // console.log("Hello World");
+let myFunction = () =>{
 
- function myFunction(){
-    console.log("Hello World");
-    let cityy = document.querySelector("#city").innerText;
-    console.log(cityy);
-    
-    axios.get('http://api.weatherapi.com/v1/current.json?key=25175e31b7074cfc895204529222906&q=karachi')
+    let cityy = document.querySelector("#city").value;
+    axios.get(`http://api.weatherapi.com/v1/current.json?key=25175e31b7074cfc895204529222906&q=${cityy}`)
     .then(function (response) {
         // handle success
         const data = response.data;
+
         console.log(data);
+
+    document.querySelector("#city_name").innerText = data.location.name + ", " + data.location.region;
+    document.querySelector("#feels_like").innerText = "Feels Like "+ data.current.feelslike_c + "°C";
+    document.querySelector("#weather_condition").innerText = data.current.condition.text;
+    document.querySelector("#weather_icon").src = data.current.condition.icon;
+    document.querySelector("#weather_condition").innerText = data.current.condition.text;
+    document.querySelector("#weather_condition").innerText = data.current.condition.text;
+    document.querySelector("#weather_condition").innerText = data.current.condition.text;
+    document.querySelector("#weather_condition").innerText = data.current.condition.text;    
+    document.querySelector("#weather_condition").innerText = data.current.condition.text;
+    
+
+
+      
+
     })
+
+
 }
 // getWeatherData();
 
