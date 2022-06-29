ec732aab90f98ccdb0f539fb75011a745b279f46
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-06-30 02:00:22 +0500

    api fetched

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-let myfinction = () => {
-    let cityy = document.querySelector("#city").value;
+// let myFunction = () =>{
+    // console.log("Hello World");
+
+ function myFunction(){
+    console.log("Hello World");
+    let cityy = document.querySelector("#city").innerText;
+    console.log(cityy);
+    
+    axios.get('http://api.weatherapi.com/v1/current.json?key=25175e31b7074cfc895204529222906&q=karachi')
+    .then(function (response) {
+        // handle success
+        const data = response.data;
+        console.log(data);
+    })
 }
+// getWeatherData();
+
+    // 25175e31b7074cfc895204529222906 key
