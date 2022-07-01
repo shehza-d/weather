ca6e29b09a4d2207b6a7c5d0cb6a4b375ca137b5
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-07-01 17:24:07 +0500

    Footer

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-let myFunction = () =>{
+let myFunction = () => {
 
     let cityy = document.querySelector("#city").value;
     axios.get(`https://api.weatherapi.com/v1/current.json?key=25175e31b7074cfc895204529222906&q=${cityy}`)
-    .then(function (response) {
-        // handle success
-        const data = response.data;
+        .then(function (response) {
+            // handle success
+            const data = response.data;
 
-        console.log(data);
-       
-       let icon = data.current.condition.icon;
-       icon.replace("/file// ");
-       console.log(icon);
-        document.querySelector("#weather_icon").src = icon;
+            console.log(data);
 
-
+            let icon = data.current.condition.icon;
+            icon.replace("/file// ");
+            console.log(icon);
+            document.querySelector("#weather_icon").src = icon;
 
 
-    document.querySelector("#tempC").innerText = data.current.temp_c + "°C";
-    document.querySelector("#tempF").innerText = "Fahrenheit : "+data.current.temp_f + "°F";
 
 
-    document.querySelector("#city_name").innerText = data.location.name + ", " + data.location.region+ ", " + data.location.country;
-    document.querySelector("#weather_condition").innerText = data.current.condition.text;
-
-    document.querySelector("#feels_like").innerText = "Feels Like: "+ data.current.feelslike_c + "°C";
-    document.querySelector("#wind_speed").innerText = "Wind Speed: " + data.current.wind_kph + "KP/H";
-    document.querySelector("#humidity").innerText = "Humidity: " + data.current.humidity + "%";
+            document.querySelector("#tempC").innerText = data.current.temp_c + "°C";
+            document.querySelector("#tempF").innerText = "Fahrenheit : " + data.current.temp_f + "°F";
 
 
-    document.querySelector("#visibility").innerText = "Visibility : " + data.current.vis_km + "KM";
-    document.querySelector("#weather_condition").innerText = data.current.condition.text;
-    document.querySelector("#weather_condition").innerText = data.current.condition.text;    
-    document.querySelector("#weather_condition").innerText = data.current.condition.text;
-    
+            document.querySelector("#city_name").innerText = data.location.name + ", " + data.location.region + ", " + data.location.country;
+            document.querySelector("#weather_condition").innerText = data.current.condition.text;
+
+            document.querySelector("#feels_like").innerText = "Feels Like: " + data.current.feelslike_c + "°C";
+            document.querySelector("#wind_speed").innerText = "Wind Speed: " + data.current.wind_kph + "KP/H";
+            document.querySelector("#humidity").innerText = "Humidity: " + data.current.humidity + "%";
 
 
-      
+            document.querySelector("#visibility").innerText = "Visibility : " + data.current.vis_km + "KM";
+            document.querySelector("#weather_condition").innerText = data.current.condition.text;
+            document.querySelector("#weather_condition").innerText = data.current.condition.text;
+            document.querySelector("#weather_condition").innerText = data.current.condition.text;
 
-    })
+
+
+
+
+        })
 
 
 }
