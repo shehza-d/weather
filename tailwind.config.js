b1efc2faa3bc87433277be59a73809f3078051c2
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-08-26 02:51:50 +0500

    screens added in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "320px",
+        mm: "375px",
+        ml: "425px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1440px",
+        "2xl": "2560px",
+      },
+      colors: {
+        accent: "var(--accent-color)",
+        accentLow: "var(--accent-low-op)",
+      },
+    },
   },
   plugins: [],
 };
