3026de88e1abcc0d3078eadddee7eb6c90bbd405
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-08-26 02:50:07 +0500

    code formatted with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import WeatherApp from "./components/weather";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import WeatherApp from "./pages/WeatherPage";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="text-5xl bg-red-500">hello</header>
+    <div className="grid min-h-screen App grid-rows-[auto,1fr,auto]">
+      <Navbar />
 
       <WeatherApp />
+
+      <Footer />
     </div>
   );
 }
