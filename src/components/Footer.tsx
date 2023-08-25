aac1538ae49a47f5801b7a5d432006a678330904
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-08-25 20:44:56 +0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,15 @@
-
 export default function Footer() {
   return (
-	<div>Footer</div>
-  )
+    <footer className="bg-accent py-1 text-center text-gray-800">
+      <a
+        // className=" hover:after:left-0 "
+        className="S_Underline relative z-10 px-1 pt-1 hover:after:w-full"
+        href="https://shehzad.vercel.app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Ⓒ Developed and Designed by Shehzad
+      </a>
+    </footer>
+  );
 }
