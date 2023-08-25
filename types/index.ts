80aa28e36532d939208e34e0b33e22b22c62803d
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-08-26 02:48:00 +0500

    response data types added
    types for response data added with
    https://www.convertsimple.com/convert-json-to-typescript/

---
+++ types/index.ts
@@ -0,0 +1,68 @@
+// https://www.convertsimple.com/convert-json-to-typescript/
+
+export interface WeatherResObject {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: DataList[];
+  city: City;
+}
+
+export interface DataList {
+  dt: number;
+  main: Main;
+  weather: Weather[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+  rain?: {
+    "3h": number;
+  };
+  city: string;
+}
+
+interface City {
+  id: number;
+  name: string;
+  coord: Coord;
+  country: string;
+  population: number;
+  timezone: number;
+  sunrise: number;
+  sunset: number;
+}
+
+interface Coord {
+  lat: number;
+  lon: number;
+}
+
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface Main {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  sea_level: number;
+  grnd_level: number;
+  humidity: number;
+  temp_kf: number;
+}
